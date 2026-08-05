3e3d5fc1a21acbd1de8e052e9174d59f2416f902
Sanatana Hub <sanatanahub18@gmail.com>
2026-08-05 19:33:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("SANATANA HUB Loaded Successfully");
+fetch("books.json")
+.then(response => response.json())
+.then(data => {
+
+const container = document.getElementById("books-container");
+
+data.forEach(book => {
+
+container.innerHTML += `
+<div class="book-card">
+
+<img src="${book.cover}" alt="${book.title}">
+
+<h3>${book.title}</h3>
+
+<p>${book.description}</p>
+
+<div class="buttons">
+
+<a href="${book.amazon}" target="_blank">Amazon</a>
+
+<a href="${book.gumroad}" target="_blank">Gumroad</a>
+
+</div>
+
+</div>
+`;
+
+});
+
+});
